d05a641140baa89dcdcb7c0d0c3d1e1f966da4a3
Muskan <sajwanmuskan123@gmail.com>
2026-09-07 13:23:11 +0530

    Updated CORS with vercel

--- rakshika-backend/main.py
+++ rakshika-backend/main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 app=FastAPI(title="Rakshika.API")
+origins=[
+    "http://localhost:3000",
+    "https://rakshika-frontend.vercel.app/"
+]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://localhost:3000"],
+    allow_origins=origins,
     allow_methods=['*'],
     allow_headers=['*'],
+    allow_credentials=True,
 )
 @app.get("/health")
 def health():
